8d08c12fc41c20d1bdd1241ba9069e2c6d560b6a
Asal Poudel <112493561+Asal99@users.noreply.github.com>
2023-10-05 12:10:29 +0545

    Add files via upload

---
+++ myproject/display.php
@@ -0,0 +1,47 @@
+<div class="container">
+	<?php include('header.php'); ?>
+	
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+<div id="create"><a href="create.php">CREATE NEW</a></div>
+<h3><a href="create.php">Create New</a></h3>
+<form action="display.php" method="post">
+	Enter Task Name <input type="text" name="txtSearch">
+	<input type="submit" name="btnSearch" class="btn btn-info" value="Search">
+</form>
+<style>
+   #create a{
+	text-decoration : none;
+	color: green;
+	font-size:32px;
+   }
+   #create{
+	background-color:aquamarine;
+	display:flex;
+	justify-content:center;
+   }
+</style>
+<?php
+	$conn=new mysqli('localhost','root','','todolist');
+	$searchtext="";
+	if(isset($_POST['btnSearch']))
+   		$searchtext = $_POST['txtSearch'];
+
+	$sql="SELECT * FROM task WHERE task_name LIKE '%$searchtext%'";
+
+
+	$result=$conn->query($sql);
+
+	$list="<table class='table'>
+			<tr>
+				<th>SN</th> <th>Task Name</th> <th>Start Date</th> <th>End Date</th> <th>User ID</th> <th>Action</th> 
+			</tr>";
+
+	$data="";
+	$s=1;
+	while($row=$result->fetch_assoc()){
+		$data=$data."<tr><td>".$s."</td><td>".$row['task_name']."</td><td>".$row['start_date']."</td><td>".$row['end_date']."</td><td>".$row['user_id']."</td><td><a href='delete.php?id=".$row['task_id']."'class='btn btn-danger'>Delete</a> <a href='edit.php?id=".$row['task_id']."'class='btn btn-success'>Edit</a></td></tr>";
+		$s++;
+	}
+	echo $list.$data.'</table>';
+
+?>
